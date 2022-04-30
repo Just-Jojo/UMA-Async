7d2e900bcdc5a1a3719ae03ee7bb83fe21753017
Just-Jojo <justjojogh@gmail.com>
2022-04-30 17:58:15 -0400

    Let's try this fr

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="uma",
+    version="1.0.0",
+    description="An async wrapper for the UMA api",
+    long_description="An async wrapper for the UMA api",
+    author="Jojo#7791",
+    license="MIT",
+    requires="aiohttp"
+)
