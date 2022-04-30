8b647be28ffaa642875b6c2aa99c446760925e34
Just-Jojo <justjojogh@gmail.com>
2022-04-30 18:05:05 -0400

    [Meta] Let's try this lmfao

--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
 from setuptools import setup
 
-setup(
-    name="uma",
-    version="1.0.0",
-    description="An async wrapper for the UMA api",
-    long_description="An async wrapper for the UMA api",
-    author="Jojo#7791",
-    license="MIT",
-    requires="aiohttp"
-)
+setup()
